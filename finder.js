53b082c2a96c6cc64a539652266a57cbaae4f8b2
William Martins <williamgmca@gmail.com>
2023-08-01 19:46:13 -0300

    correção codigo

--- finder.js
+++ finder.js
@@ -15,18 +15,26 @@ async function getUSer() {
     //getRepos();
     const URL = `https://api.github.com/users/${QUERY.value}`;
     const DATA = await fetch(URL);
-    const USER_DATA = await DATA.json;
-    if (USER_DATA.message === "Not Found")
-    alert("User not Found")
+    const USER_DATA = await DATA.json();
+    
+    if (USER_DATA.message === "Not Found") {
+    alert("User not Found");
     return;
+    }
+
+    USER.login = USER_DATA.login;
+    USER.name = USER_DATA.name;
+    USER.bio =USER_DATA.bio;
+    USER.avatar_url = USER_DATA.avatar_url;
+    USER.public_repos = USER_DATA.public_repos;
+    USER.followers = USER_DATA.followers;
+    USER.following = USER_DATA.following;
+
+    console.table(USER);
+
+    //getAllData()
 }
 
-USER.login = USER_DATA.login;
-USER.name = USER_DATA.name;
-USER.bio =USER_DATA.bio;
-USER.avatar_url = USER_DATA.avatar_url;
-USER.public_repos = USER_DATA.public_repos;
-USER.followers = USER_DATA.followers;
-USER.following = USER_DATA.following;
-
-console.table(USER);
+async function getRepos() {
+    const URL = `https://api.github.com/users/`
+}
