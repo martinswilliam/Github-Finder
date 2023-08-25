61b29a50de7f1b2197273ccbf550735cde95660a
William Martins <williamgmca@gmail.com>
2023-08-25 19:06:19 -0300

    finalização projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# github-finder 🚀
+
+Projeto realizado através do Bootcamp Java Script Developer da DIO
+
+## Sobre o projeto 🔎
+
+Consiste numa app que recupera informações de usuários do github, passando o nome de usuário como parâmetro, retornando os dados como por exemplo: name; login; public repos...
+
+
--- finder.js
+++ finder.js
@@ -1,4 +1,4 @@
-const USER = {
+const user = {
     login: String,
     name: String,
     bio: String,
@@ -7,34 +7,91 @@ const USER = {
     followers: Number,
     following: Number,
     repos: Array,
-}
-
-const QUERY = document.querySelector(".query");
-
-async function getUSer() {
-    //getRepos();
-    const URL = `https://api.github.com/users/${QUERY.value}`;
-    const DATA = await fetch(URL);
-    const USER_DATA = await DATA.json();
-    
-    if (USER_DATA.message === "Not Found") {
-    alert("User not Found");
-    return;
+  };
+  const query = document.querySelector(".query");
+  
+  async function getUser() {
+    getRepos();
+    const url = `https://api.github.com/users/${query.value}`;
+    const data = await fetch(url);
+    const userData = await data.json();
+  
+    if (userData.message === "Not Found") {
+      alert("User not found");
+      return;
     }
-
-    USER.login = USER_DATA.login;
-    USER.name = USER_DATA.name;
-    USER.bio =USER_DATA.bio;
-    USER.avatar_url = USER_DATA.avatar_url;
-    USER.public_repos = USER_DATA.public_repos;
-    USER.followers = USER_DATA.followers;
-    USER.following = USER_DATA.following;
-
-    console.table(USER);
-
-    //getAllData()
-}
-
-async function getRepos() {
-    const URL = `https://api.github.com/users/`
-}
+  
+    user.login = userData.login;
+    user.name = userData.name;
+    user.bio = userData.bio;
+    user.avatar_url = `https://github.com/${userData.login}.png`;
+    user.public_repos = userData.public_repos;
+    user.followers = userData.followers;
+    user.following = userData.following;
+  
+    getAllData();
+  }
+  
+  async function getRepos() {
+    const url = `https://api.github.com/users/${query.value}/repos`;
+    const data = await fetch(url);
+    const repos = await data.json();
+    user.repos = repos;
+  }
+  
+  function getAllData() {
+    const userInfo = document.getElementById("user-info");
+  
+    userInfo.setAttribute("style", "display: block");
+    userInfo.innerHTML = `
+      <div class="user-info">
+        <div class="user-avatar">
+            <h2 class="user-name">${user.name == null ? user.name = "No username" : user.name}</h2>
+            <h3 class="user-login">${user.login}</h3>
+            <img class="avatar_img" src="${user.avatar_url}" alt="${user.login}">
+            <p class="bio">${
+              user.bio == null ? (user.bio = "No bio") : user.bio
+            }</p>
+        </div>
+        <div class="user-stats">
+            <h2>Info</h2>
+            <p>Repos: ${user.public_repos}</p>
+            <p>Followers: ${user.followers}</p>
+            <p>Following: ${user.following}</p>
+        </div>
+      </div>
+      ${user.repos
+        .map(
+          (repo) => `<div class="repo">
+        <h3 class="repo-name">${repo.name}</h3>
+        <p class="repo-description">${
+          repo.description == null
+            ? (repo.description = "No description")
+            : repo.description
+        }</p>
+        <p class="repo-language">${repo.language == null ? repo.language = "No language found" : repo.language}</p>
+        <div class="repo-stats">
+          <span>
+            ⭐${repo.stargazers_count}
+          </span>
+          <span>
+            💬${repo.watchers_count}
+          </span>
+          <span>
+            📦${repo.forks_count}
+          </span>
+          <span>
+            📅${new Date(repo.updated_at).toLocaleDateString('pt-br')}
+          </span>
+        </div>
+        <a href="${repo.html_url}" target="_blank">
+        <span>
+        🔗${repo.html_url}
+        </span>
+        </a>
+      </div>`
+        )
+        .join("")}
+    `;
+  }
+  
